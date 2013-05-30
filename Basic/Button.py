4e96274aaf4e8225f8ad1693316101921b753fee
Ling Yuan <cmmy82@hotmail.com>
2013-05-31 00:50:08 +0800

    change UI(button)

--- Basic/Button.py
+++ Basic/Button.py
@@ -5,12 +5,12 @@ class button(wx.Button):
     def __init__(self, panel, text, pos, size, cbk = None, bid = 0):
         wx.Button.__init__(self, panel, -1, text, pos=pos, size=size)
         if (cbk != None):
-            self.Bind(wx.EVT_BUTTON, self.OnClick, self)
+            self.Bind(wx.EVT_BUTTON, self.__onClick, self)
         self.click_cbk = cbk
         self.bid = bid
         
         
-    def OnClick(self, event):
+    def __onClick(self, event):
         if self.click_cbk.func_code.co_argcount == 1:
             self.click_cbk()
         else:
