61ba51adc37ee48a8f5115f613b5c98c13f1c0c2
Ling Yuan <cmmy82@hotmail.com>
2013-06-01 01:54:17 +0800

    add function in logcat

--- Tabs/Modules/Logcat.py
+++ Tabs/Modules/Logcat.py
@@ -9,17 +9,44 @@ class moduleFrame(module):
         self.createButton('start',  1, 0, 10, self.onLogcatStart)
         self.createButton('stop', -1, 0, 10, self.onLogcatStop)
         self.createButton('touch', -1, 0, 10, self.onLogcatTouch)
+        self.createButton('open', -1, 0, 10, self.onLogcatOpen)
+
+        self.modules = [['ActivityManager'],
+                  ['WindowManager']]
+
+        self.files = ['Temp\\ams.log',
+                 'Temp\\wms.log']
+
+        self.createButton('ams', 1, 1, 10, self.onShowLogcat, 0) #AMS
+        self.createButton('wms', -1, 1, 10, self.onShowLogcat, 1) #WMS
         
         self.ar = android(self.frame)
 
+        self.logcatFile = None
+        self.logcatFinish = True
+
     def onLogcatStart(self):
-        self.ar.startLogcat('Temp\\log.log')
+        self.logcatFile = 'Temp\\log.log'
+        self.logcatFinish = False
+        self.ar.startLogcat(self.logcatFile)
+        
+    def onLogcatOpen(self):
+        if self.logcatFinish == True:
+            self.logcatFile = self.frame.doFileDialog()
         
     def onLogcatStop(self):
         if self.ar.stopLogcat() == True:
-            self.ar.filterLogcat('Temp\\log.log', 'Temp\\logs.log', ['PhoneStatusBar','ActivityManager'], ['389'])
-            inst = misc.getInstance()
-            inst.openFile('Temp\\logs.log', 'log')            
+            self.logcatFinish = True
+            pass
     
     def onLogcatTouch(self):
-        self.ar.touchLogcat()
+        if self.logcatFinish == False:
+            self.ar.touchLogcat()
+        
+    def onShowLogcat(self, mid):
+        if self.logcatFile != None and self.logcatFinish == True:
+            self.ar.filterLogcat(self.logcatFile, self.files[mid], self.modules[mid])
+            inst = misc.getInstance()
+            inst.openFile(self.files[mid], 'log')
+            
+
