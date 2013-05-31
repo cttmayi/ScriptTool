52bad3476e06b468669ac9539175ea74dd1c4c10
Ling Yuan <cmmy82@hotmail.com>
2013-05-31 23:57:29 +0800

    fix logcat pos

--- Tabs/Modules/Logcat.py
+++ Tabs/Modules/Logcat.py
@@ -1,22 +1,25 @@
 
 from Basic.Module import module
+from Util.Misc import misc
 
 from Function.Android import android
 
 class moduleFrame(module):
     def onCreate(self):
-        self.createButton('logcat',  1, 2, 10, self.onLogcat)
-        self.createButton('logcat stop', -1, 2, 10, self.onLogcatStop)
-        self.createButton('logcat touch', -1, 2, 10, self.onLogcatTouch)
+        self.createButton('start',  1, 0, 10, self.onLogcatStart)
+        self.createButton('stop', -1, 0, 10, self.onLogcatStop)
+        self.createButton('touch', -1, 0, 10, self.onLogcatTouch)
         
         self.ar = android(self.frame)
 
-    def onLogcat(self):
+    def onLogcatStart(self):
         self.ar.startLogcat('Temp\\log.log')
         
     def onLogcatStop(self):
         if self.ar.stopLogcat() == True:
             self.ar.filterLogcat('Temp\\log.log', 'Temp\\logs.log', ['PhoneStatusBar','ActivityManager'], ['389'])
+            inst = misc.getInstance()
+            inst.openFile('Temp\\logs.log', 'log')            
     
     def onLogcatTouch(self):
         self.ar.touchLogcat()
