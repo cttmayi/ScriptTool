f00ba54a415eddf509b9fa3ea47e6f36dc7daacc
Ling Yuan <cmmy82@hotmail.com>
2013-07-20 21:11:14 +0800

    add example

--- Tabs/Modules/Module1.py
+++ Tabs/Modules/Module1.py
@@ -6,9 +6,12 @@ class moduleFrame(module):
     def onCreate(self):
         #self.createStatic("Name:", 0, 0)
         
-        self.createButton("RunCmdCbk", 0, 0, 15, None)
+        self.createButton("RunCmdCbk", 0, 0, 15, None, eCbk = self.onButtonShow)
         self.createTable(-1, 0, 60, 10, ['A','B','C'])
         self.systraceOption = ['gfx', 'input', 'view', 'wm', 'am', 'sync', 'audio', 'video', 'camera']
         self.systraceCBok = self.createCheckbox(0, -1, 75, self.systraceOption, len(self.systraceOption))
         
         pass
+    
+    def onButtonShow(self):
+        return False
